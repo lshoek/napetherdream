71254355e2e270f3d2a5772576fae70e39553394
lshoek <lesleyvanhoek@hotmail.com>
2025-10-30 11:11:50 +0100

    fix targeting of platform-specific source files - remove macos target

--- module_extra.cmake
+++ module_extra.cmake
@@ -2,10 +2,16 @@ if(NOT TARGET etherdream)
     find_package(etherdream REQUIRED)
 endif()
 
-filter_platform_specific_files(SOURCES)
-add_platform_specific_files("${WIN32_SOURCES}" "${MACOS_SOURCES}" "${LINUX_SOURCES}")
-filter_platform_specific_files(HEADERS)
-add_platform_specific_files("${WIN32_SOURCES}" "${MACOS_SOURCES}" "${LINUX_SOURCES}")
+# Target filtered platform-specific source files
+file(GLOB_RECURSE ETHERDREAM_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp)
+filter_platform_specific_files(ETHERDREAM_SOURCES)
+add_platform_specific_files("${WIN32_SOURCES}" "${LINUX_SOURCES}")
+
+if(MSVC)
+	target_sources(${PROJECT_NAME} PRIVATE ${WIN32_SOURCES})
+else()
+	target_sources(${PROJECT_NAME} PRIVATE ${LINUX_SOURCES})
+endif()
 
 target_include_directories(${PROJECT_NAME} PUBLIC ${ETHERDREAM_INCLUDE_DIR})
 
